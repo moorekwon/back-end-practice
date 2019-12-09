3945481ec2390b00aec286bfa1147b4b360e8332
Hyojin Kwon <raccoonhj33@gmail.com>
2019-12-09 21:47:05 +0900

    191209 crawling basic scraping - lxml

--- fconline/crawling/크롤링기초_코드/section02-4-c.py
+++ fconline/crawling/크롤링기초_코드/section02-4-c.py
@@ -19,7 +19,45 @@ def main():
     urls = scrape_news_list_page(response)
 
     # 딕셔너리 확인
-    # print(urls)
+    # prort requests
+# import lxml.html
+
+# def main():
+#     """
+#     네이버 메인 뉴스 스탠드 스크랩핑 메인 함수
+#     """
+    
+#     # 스크랩핑 대상 URL
+#     response = requests.get('https:www.naver.com/')
+    
+#     # 신문사 링크 리스트 획득
+#     urls = scrape_news_list_page(response)
+    
+#     # 결과 출력
+#     for url in urls:
+#         print(url)
+        
+# def scrape_news_list_page(response):
+#     # URL 리스트 선언
+#     urls = []
+    
+#     # 태그 정보 문자열 저장
+#     root = lxml.html.fromstring(response.content)
+    
+#     # 문서 내 경로 절대 경로 변환
+#     root.make_links_absolute(response.url)
+    
+#     for a in root.cssselect('.api_list .api_item a.api_link'):
+#         # link
+#         url = a.get('href')
+        
+#         # 리스트 삽입
+#         urls.append(url)
+#     return urls
+
+# # 스크랩핑 시작
+# if __name__ == '__main__':
+#     main()int(urls)
 
     # 결과 출력
     for name, url in urls.items():
